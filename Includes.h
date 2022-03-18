6a5f880bf5f490d1c80567ffdd423a1b0302e41b
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-03-18 22:55:51 +0100

    Add files via upload

---
+++ Includes.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "winmm.lib")
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <Windows.h>
+#include <gdiplus.h>
+#include <thread>
+#include <math.h>
+
+
+#include "Headers/Types.h"
+#include "Headers/Linkage.h"
+#include "Headers/Render.h"
+#include "Headers/Window.h"
+#include "Headers/Functional.h"
+#include "Headers/Line.h"
+
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "Includes.h"
+
+int main() {
+
+	_window_size_x = 1000;
+	_window_size_y = 1000;
+	_render_buffer = new unsigned int[_window_size_x * _window_size_y];
+	_background_color = Color(255, 255, 255);
+	wndCreate("Anti-Aliased-Line", WS_OVERLAPPEDWINDOW);
+
+	bool toggle = true;
+
+	for (;;) {
+		clearPArray();
+		if (GetAsyncKeyState('T') & 0x8000) {
+			toggle = !toggle;
+			Sleep(300);
+		}
+		if (toggle) {
+			DrawLine({ 500,500 }, MousePosition(), Color(255, 0, 0));
+		}
+		else {
+			DrawAALine({ 500,500 }, MousePosition(), Color(255, 0, 0));
+		}
+		displayPArray();	
+	}
+}
