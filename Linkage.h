04a9631fcfed16f3e82e87256a93c4a40ed62d0b
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-03-19 01:01:13 +0100

    Add files via upload

---
+++ Linkage.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../Includes.h"
+
+extern HWND _window_hwnd;
+
+extern WNDCLASSEX _window_class;
+ 
+extern HDC dc;
+
+extern unsigned int* _render_buffer;
+
+extern unsigned int _background_color;
+
+extern unsigned int _window_size_x;
+
+extern unsigned int _window_size_y;
