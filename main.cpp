7171d0fb108551cd68d81823a70eebe4b2780fae
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-03-19 01:01:02 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,18 +9,22 @@ int main() {
 	wndCreate("Anti-Aliased-Line", WS_OVERLAPPEDWINDOW);
 
 	bool toggle = true;
-
+	int iterations = 1;
 	for (;;) {
 		clearPArray();
 		if (GetAsyncKeyState('T') & 0x8000) {
 			toggle = !toggle;
 			Sleep(300);
 		}
+		if (GetAsyncKeyState('I') & 0x8000) {
+			iterations++;
+			Sleep(300);
+		}
 		if (toggle) {
 			DrawLine({ 500,500 }, MousePosition(), Color(255, 0, 0));
 		}
 		else {
-			DrawAALine({ 500,500 }, MousePosition(), Color(255, 0, 0));
+			DrawAALine({ 500,500 }, MousePosition(), Color(255, 0, 0), iterations);
 		}
 		displayPArray();	
 	}
